7b1502dc9620ef06a7560b725c07eaa615191217
InsulatingTape <flimrendelys@gmail.com>
2025-01-28 21:43:53 +0300

    Update orexi.py

--- задачи2.2/orexi.py
+++ задачи2.2/orexi.py
@@ -79,7 +79,7 @@ elif (ap2 + M) > (ap1 + N):
 F
 
 year = int(input())
-if (year % 4 == 0) and (year % 100 != 0):
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
     print('YES')
 else:
     print('NO')
